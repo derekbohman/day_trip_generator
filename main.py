f6c15f12c3b13cabb3f87e0d936f677a093c2cb2
Derek Bohman <derekbohman@Dereks-MBP.hsd1.or.comcast.net>
2022-03-12 09:12:17 -0800

    Completed the basic version. Will improve later.

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 import random
 
 destinations = ["Germany", "Iceland", "Italy", "Norway", "Sweden"]
-entertainments = ["Hike", "Museum", "Relax", "Shop", "Theater"]
-modes_of_transportations = ["Boat", "Plane", "Taxi", "Train", "Walk"]
+entertainments = ["Exploring", "Hiking", "Relaxing", "Shopping",]
+modes_of_transportation = ["Boat", "Plane", "Taxi", "Train"]
 restaurants = ["Germanic", "Italian", "Scandinavian", "Local Favorite"]
 
-def destination_randomizer(destinations):
+def destinations_randomizer(destinations):
     destination_result = random.choice(destinations)
     return destination_result
 
-def entertainment_randomizer(entertainments):
+def entertainments_randomizer(entertainments):
     entertainment_result = random.choice(entertainments)
     return entertainment_result
 
-def mode_of_transportation_randomizer(modes_of_transportation):
+def modes_of_transportation_randomizer(modes_of_transportation):
     mode_of_transportation_result = random.choice(modes_of_transportation)
     return mode_of_transportation_result
 
@@ -23,9 +23,9 @@ def restaurants_randomizer(restaurants):
     restaurant_result = random.choice(restaurants)
     return restaurant_result
 
-destination_result = destination_randomizer(destinations)
-entertainment_result = entertainment_randomizer(entertainments)
-mode_of_transportation_result = mode_of_transportation_randomizer(modes_of_transportations)
+destination_result = destinations_randomizer(destinations)
+entertainment_result = entertainments_randomizer(entertainments)
+mode_of_transportation_result = modes_of_transportation_randomizer(modes_of_transportation)
 restaurant_result = restaurants_randomizer(restaurants)
 
 print("")
@@ -34,7 +34,7 @@ print("")
 destination_choice = input("Type 'Yes if you agree or 'No' if you disagree. ")
 
 while destination_choice != "Yes":
-    destination_result = destination_randomizer(destinations)
+    destination_result = destinations_randomizer(destinations)
     print("")
     print("We're sorry. How about " + destination_result + " ?")
     print("")
@@ -50,6 +50,7 @@ print("")
 entertainment_choice = input("Type 'Yes if you agree or 'No' if you disagree. ")
 
 while entertainment_choice != "Yes":
+    entertainment_result = entertainments_randomizer(entertainments)
     print("")
     print("We're sorry. How about " + entertainment_result + " ?")
     print("")
@@ -65,6 +66,7 @@ print("")
 mode_of_transportation_choice = input("Type 'Yes if you agree or 'No' if you disagree. ")
 
 while mode_of_transportation_choice != "Yes":
+    mode_of_transportation_result = modes_of_transportation_randomizer(modes_of_transportation)
     print("")
     print("We're sorry. How about " + mode_of_transportation_result + " ?")
     print("")
@@ -80,6 +82,7 @@ print("")
 restaurant_choice = input("Type 'Yes if you agree or 'No' if you disagree. ")
 
 while restaurant_choice != "Yes":
+    restaurant_choice = restaurants_randomizer(restaurants)
     print("")
     print("We're sorry. How about " + restaurant_result + " ?")
     print("")
@@ -88,3 +91,24 @@ else:
     print("")
     print("Congratulations on choosing your dining option. Let's move on.")
     print("")
+
+print("")
+print("Congratulations. We have completed generating your day trip.")
+print("")
+print("Now, let's confirm that this is the trip you want.")
+print("")
+print("The trip that we have generated for you is:")
+print("Destination: " + destination_choice)
+print("Entertainment: " + entertainment_choice)
+print("Mode of transportation: " + mode_of_transportation_choice)
+print("Restaurant: " + restaurant_choice)
+print("")
+
+user_confirmation = input("Type 'Yes' to confirm or 'No' to make changes.")
+
+if user_confirmation == "Yes":
+    print("Get ready for the adventure of a lifetime. You will be arriving in " + destination_choice + \
+    " by " + mode_of_transportation_choice + " where you will spend the day " + entertainment_choice + ". " + \
+    "You will end the day by enjoying a wonderful " + restaurant_choice + " meal.")
+else:
+    print("We apologize, but that's what's happening. Have fun!")
